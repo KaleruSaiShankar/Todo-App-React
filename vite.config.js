bd9e84f4d8b3ce742cde75f13f125dc477d1c346
Sai Shankar <162856945+KaleruSaiShankar@users.noreply.github.com>
2025-05-26 18:23:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Todo-App-React/', // 🔁 Replace with actual GitHub repo name
   plugins: [react()],
 })
